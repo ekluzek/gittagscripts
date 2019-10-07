a4164b17f878df9c7df7b430b0de15d1026c0199
Erik Kluzek <erik@ucar.edu>
2019-10-07 13:58:37 -0600

    Add a test script that doesn't email

---
+++ git_tag_test.pl
@@ -0,0 +1,196 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use Data::Dumper;
+use Array::Utils qw(:all);
+use Log::Log4perl;
+use Cwd;
+use MIME::Base64;
+use Net::GitHub;
+
+
+my $gh = Net::GitHub::V3->new(
+);
+
+# Globals
+# PLEASE make sure this is set to the absolute path of the tagList file..
+my $user = $ENV{'USER'};
+my $test = 0; 
+#  $test = 1;    # uncomment to run a test and send email to admin rather than standard
+
+my $tagList = "/home/$user/tagscripts/gittagList.txt";
+my $logConfig = "/home/$user/tagscripts/tagmailer.log.conf";
+my $adminEmail = 'erik@ucar.edu';
+
+my $clmTagEmail;
+my $relTagEmail;
+if ( ! $test ) {
+   $relTagEmail = 'ctsm-dev@ucar.edu';
+   $clmTagEmail = 'ctsm-dev@ucar.edu';
+} else {
+   $relTagEmail = 'erik@ucar.edu';
+   $clmTagEmail = 'erik@ucar.edu';
+}
+
+
+my $clmName = 'CTSM Gatekeeper';
+my $relName = 'CTSM Release Candidate Gatekeeper';
+my $gitRepoPath = "/repos/ESCOMP/ctsm/";
+my $tagsSent;
+my $maxTagCount = 8;
+
+Log::Log4perl->init($logConfig);
+my $log = Log::Log4perl->get_logger("TAGMAILER");
+my $cwd = getcwd();
+$log->debug("Current working directory: $cwd");
+
+
+sub checkNewTags
+{
+    my @currentTags;
+    if (! -f $tagList)
+    {
+	$log->fatal("Tag list not found: $tagList");
+	adminError("Tag list not found: $tagList");
+    }
+    else
+    {
+	# Get the stored list of tags. 
+	open my $TAGS, "<", $tagList or die $!;
+	@currentTags = <$TAGS>;
+	chomp @currentTags;
+	close $TAGS;
+    }
+    # get the current list of all tags
+    my @GitTagResults = $gh->query('GET', "${gitRepoPath}tags?per_page=101");
+    my @newFoundTags=("blank");
+    foreach my $d  (@GitTagResults){
+       my $tag=$d->{'name'};
+       push @newFoundTags,$tag;
+    }
+    shift @newFoundTags;
+    print Dumper \@newFoundTags;
+}
+
+sub updateTagList
+{
+    open my $TAGS, ">", $tagList or die $!;
+    foreach my $d  (@_){
+      print $TAGS "$d\n";
+    }
+    close $TAGS;
+}
+
+
+sub checkSentList
+{
+}
+
+sub updateSentList
+{
+
+}
+
+sub mailChangeLog
+{
+    my $tagName  = shift;
+
+    my $changefile = "ChangeLog";
+    if ( $tagName =~ /release-clm5.0/ ) {
+      $changefile = "release-clm5.0.ChangeLog";
+    }
+    # Get sha of $tagName ChangeLog
+    my $gitTagcmd = $gitRepoPath.'contents/doc/?ref='.$tagName;
+    my @tagContents = $gh->query("GET",$gitTagcmd);
+    my $sha = undef;
+    foreach my $d  (@tagContents){
+      print $d->{'name'} . "\n";
+      print $d->{'html_url'} . "\n";
+      if ($d->{'name'} eq "$changefile") {
+        $sha=$d->{'sha'};
+      }
+    }
+    if ( ! defined($sha) ) {
+        # This is a total kludgy hack!, use a known sha
+        $sha = "1bbae402996e1d5238b4059da24976ceee875f0f";
+	#adminError("Could NOT find the $changefile file!!");	
+    }
+    my $gitChangeLogcmd= $gitRepoPath."git/blobs/".$sha;
+    my $gitChangeLog = $gh->query('GET',$gitChangeLogcmd);
+
+    my $WholeChangeLog = decode_base64($gitChangeLog->{'content'});
+    
+    chomp $WholeChangeLog;
+    my $tagChangeLog = cutChangeLog(split "\n",$WholeChangeLog);
+    
+    my $tagAddress;
+    my $subject;
+    $tagAddress = $clmTagEmail if $tagName =~ /(ctsm)/i;
+    $ENV{'NAME'}  =  $clmName if $tagName =~ /^ctsm/i;
+    $ENV{'NAME'}  =  $relName if $tagName =~ /^release-clm/i;
+    if($tagName =~ /^ctsm/i)
+    {
+	$tagAddress = $clmTagEmail;
+	$subject = $tagName;
+	$ENV{'NAME'} = $clmName;
+    }
+    if($tagName =~ /^release-clm/i)
+    {
+	$tagAddress = $clmTagEmail;
+	$subject = $tagName;
+	$ENV{'NAME'} = $relName;
+    }
+    
+    my $mailcmd = "| mailx -s '$subject' $tagAddress";
+    $log->debug("mail cmd: $mailcmd");
+    $log->info("mailing changelog for tag: $tagName");
+    open MAIL, "$mailcmd";
+    print MAIL  $tagChangeLog;
+    close MAIL;
+}
+
+
+sub cutChangeLog
+{
+    my @wholeChangeLog = @_;
+    my $tagChangeLog = "*** RESPONSES TO THIS EMAIL WILL NOT BE READ ***\n";
+    my $endFound = 0;
+    foreach my $line(@wholeChangeLog)
+    {
+	if($line =~ /Tag name:/i)
+	{
+	    if($endFound == 1)
+	    {
+		last;
+	    }
+	    else
+	    {
+		$endFound = 1;
+	    }
+	}
+	$tagChangeLog .= "$line\n";
+    }
+    return $tagChangeLog;
+}
+
+sub adminError
+{
+    my $error = shift;
+    $log->fatal($error);
+    my $message = "Something went wrong! error was: \n";
+    $message .= $error;
+    open MAIL, "| mailx -s 'tagmailer errors found..'  $adminEmail";	
+    print MAIL $message;
+    close MAIL;
+    exit(1);
+}
+
+
+sub main
+{
+    checkNewTags();
+}
+
+main(@ARGV) unless caller;
+
+
